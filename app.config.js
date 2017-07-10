ec83b3f9014a88ce09d1559c73da15d5c04c292b
Ryan Tate <ryan.michael.tate@gmail.com>
2017-07-10 10:17:01 -0700

    Updating web3 endpoint

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  web3Provider: "http://138.68.225.133:8545",
+  web3Provider: "https://gittoken.org/web3/",
   socketServer: 'wss://gittoken.org/ws/',
   contractAddress: '0xedc2fe67c31c2aca5cf5866b2208fe1ec113e8c8'
 }
